49e69d2e23e0978e81a640494d1105a1f6251758
MohiniKshirsagar <73096764+MohiniKshirsagar@users.noreply.github.com>
2021-05-05 05:03:50 -0700

    Partials Folder Files
    Inside Online Folder create another two folders named as Partials , img. Partials will contain all Partials folder files. And all images will be in img folder.

---
+++ _dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+// script to connect to the database:
+$servername= "localhost";
+$username = "root";
+$password = "";
+$database = "iDiscuss";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+
+
+
+
+
+?>
+
---
+++ _footer.php
@@ -0,0 +1,6 @@
+<div class="container-fluid bg-dark text-light">
+<P class="text-center mb-0">Copyright iDiscuss Coding Forums 2021 | All rights reserved</p>
+
+
+
+</div>
